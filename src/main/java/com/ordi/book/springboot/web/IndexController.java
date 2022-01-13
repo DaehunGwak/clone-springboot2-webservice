79d3b71586d25acc57e018bbcbd8adcdd2cc9505
DaehunGwak <rhkrgns56@naver.com>
2022-01-13 23:53:05 +0900

    feat: 로그인 유저 어노테이션 기반으로 개선

--- src/main/java/com/ordi/book/springboot/web/IndexController.java
+++ src/main/java/com/ordi/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ordi.book.springboot.web;
 
+import com.ordi.book.springboot.config.auth.LoginUser;
 import com.ordi.book.springboot.config.auth.dto.SessionUser;
 import com.ordi.book.springboot.service.PostsService;
 import com.ordi.book.springboot.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (Objects.nonNull(user)) {
             model.addAttribute("userName", user.getName());
         }
